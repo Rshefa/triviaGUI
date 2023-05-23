0ecc6955dc2b214196dd415d4f21ad758772e154
Rshefa <raisas@nycstudents.net>
2023-05-23 11:01:31 -0400

    :)

---
+++ src/Dogs.java
@@ -0,0 +1,16 @@
+public class Dogs {
+
+    private String breed;
+
+
+    public Dogs(String breed) {
+        this.breed = breed;
+
+    }
+
+    public String getName() {
+        return breed;
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        Dogs dog = new Dogs("poodle");
+        System
+       // MyWeatherGUI gui = new MyWeatherGUI();
+    }
+
+}
